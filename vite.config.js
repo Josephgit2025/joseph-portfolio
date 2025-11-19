06f28a5b73f29fdadc754f9bd3f304320b4793f7
Josephgit2025 <joseph-marie.bile@epitech.eu>
2025-11-19 15:40:55 +0100

    Fix base URL for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: '/joseph-portfolio-public/',
+  base: '/joseph-portfolio/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
